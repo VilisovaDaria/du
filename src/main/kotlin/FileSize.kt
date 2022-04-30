46e9ae9f99cd2036850ebb2212d17e8f3f0846be
VilisovaDaria <daria.v2906@gmail.com>
2022-04-30 13:44:24 +0300

    +

--- src/main/kotlin/FileSize.kt
+++ src/main/kotlin/FileSize.kt
@@ -3,61 +3,32 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 
-fun du(h: Boolean, c: Boolean, si: Boolean, vararg inputName: File): String {
+fun du(humanReadable: Boolean, totalSize: Boolean, foundation: Boolean, vararg inputName: File): String {
     var summarySize = 0.toLong()
 
     for (i in inputName.indices) {
         if (!inputName[i].exists()) throw IllegalArgumentException("The file does not exist")
     }
 
+    if (humanReadable) return getSize(totalSize, foundation, *inputName).toString()
 
-    if (h) return getSize(c, si, *inputName).toString()
-    if (c) {
-        var baseFoundation = 1024
-        if (si) baseFoundation = 1000
-        for (i in inputName.indices) {
-            val arrayBytesSize = unpacking(inputName[i]).values.toLongArray()
-            for (k in arrayBytesSize.indices){
+    //здесь вывод в КВ и без остатка, так как дробная часть не играет большую роль, как например в GB
+    var baseFoundation = 1024
+    if (foundation) baseFoundation = 1000
+    for (i in inputName.indices) {
+        val arrayBytesSize = unpacking(inputName[i]).values.toLongArray()
+        for (k in arrayBytesSize.indices){
+            if (totalSize) {
                 summarySize += arrayBytesSize[k]
-            }
-        }
-        return (summarySize / baseFoundation).toString()
-    } else {
-        var baseFoundation = 1024
-        if (si) baseFoundation = 1000
-        for (i in inputName.indices) {
-            val arrayBytesSize = unpacking(inputName[i]).values.toLongArray()
-            for (k in arrayBytesSize.indices){
-                return (arrayBytesSize[k] / baseFoundation).toString()
-            }
+            } else return (arrayBytesSize[k] / baseFoundation).toString()
         }
     }
-    return ""
+
+    return (summarySize / baseFoundation).toString()
 }
 
 
-fun unpacking(inputName: File): MutableMap<String, Long> {
-    val result = mutableMapOf<String, Long>()
-
-    if (inputName.isDirectory) {
-        val listOfSummaryFileSize = mutableListOf<File>()
-        val sizeOfOneFile = inputName.walkTopDown().filter { it.isFile }.forEach { listOfSummaryFileSize.add(it) }
-        var summary = 0.toLong()
-
-        for (i in 0..listOfSummaryFileSize.lastIndex){
-            val d = Files.size(Paths.get(listOfSummaryFileSize[i].toString()))
-            summary += d
-        }
-        result[inputName.toString()] = summary
-    }
-    else {
-        result[inputName.toString()] = Files.size(Paths.get(inputName.toString()))
-    }
-    return result
-}
-
-
-//функция, если "-h" - true
+//функция, если опция "-h" - true
 fun getSize(totalSize: Boolean, foundation: Boolean, vararg inputName: File): List<String> {
     val result = mutableListOf<String>()
     val listFileSizeByte = mutableListOf<Long>()
@@ -66,12 +37,14 @@ fun getSize(totalSize: Boolean, foundation: Boolean, vararg inputName: File): Li
     var summarySize = 0.toLong()
     var baseFoundation = 1024
 
+    //если опция "-с" (суммарный размер) false
     if (!totalSize) {
         for (i in inputName.indices) {
             listFileSizeByte.addAll(unpacking(inputName[i]).values)
             listFileName.addAll(unpacking(inputName[i]).keys)
         }
     }
+    // если true
     if (totalSize) {
         for (i in inputName.indices) {
             val arrayBytesSize = unpacking(inputName[i]).values.toLongArray()
@@ -83,24 +56,26 @@ fun getSize(totalSize: Boolean, foundation: Boolean, vararg inputName: File): Li
         listFileName.add("Summary size")
     }
 
-
+    // применяем опцию "-h"
     if (foundation) baseFoundation = 1000
     for (i in listFileSizeByte.indices){
-        var k = 0
-        val type = arrayOf("B", "KB", "MB", "GB")
-
-        if (listFileSizeByte[i] < baseFoundation) result.addAll(listOf("${listFileName[i]} - ${listFileSizeByte[i]} B}"))
-
-        else {
-            while (listFileSizeByte[i] / baseFoundation != 0.toLong() && i < type.size - 1 ) {
-                listFileSizeByte[i] /= baseFoundation.toLong()
-                k ++
-            }
-
-            val divisor = (listFileSizeByte[i] - k * baseFoundation) % baseFoundation
-            result.addAll(listOf("${listFileName[i]} - ${listFileSizeByte[i]}.$divisor ${type[k]}"))
-        }
+        result.addAll(listOf("${listFileName[i]} - ${humanReadable(listFileSizeByte[i])}"))
     }
     return result
 }
 
+
+fun humanReadable(k: Long): String {
+    var i = 0
+    val type = arrayOf("B", "KB", "MB", "GB")
+    var n = k
+    if (n < 1024) return "$n B"
+
+    while (n / 1024 > 0) {
+        n /= 1024
+        i += 1
+    }
+
+    val divisor = (k - n * 1024) % 1024
+    return "$n.$divisor ${type[i]}"
+}
