0c40ff52a706a5b8146292f9c7223c04ceca853a
VilisovaDaria <daria.v2906@gmail.com>
2022-05-06 21:15:08 +0300

    +

--- src/main/kotlin/FileSize.kt
+++ src/main/kotlin/FileSize.kt
@@ -1,47 +1,28 @@
 import java.io.*
-import java.lang.String.format
-import java.math.MathContext
-import java.nio.file.Files
-import java.nio.file.Paths
 
-fun du(humanReadable: Boolean, totalSize: Boolean, foundation: Boolean, vararg inputName: File): List<String> {
-    val listFileSizeByte = mutableListOf<Long>()
-    val listFileName = mutableListOf<String>()
+
+fun du(humanReadable: Boolean, totalSize: Boolean, foundation: Boolean, vararg inputFile: File): Map<String, String> {
+    val result = mutableMapOf<String, String>()
     var summarySize = 0L
-    val result = mutableListOf<String>()
+    val baseFoundation = if (foundation) 1000 else 1024
 
-    if (humanReadable && !totalSize || !humanReadable) {
-        for (i in inputName.indices) {
-            unpacking(inputName[i]).forEach { listFileName.add(it.key) && listFileSizeByte.add(it.value) }
-        }
-    }
 
-    if (humanReadable) {
-        if (totalSize) {
-            for (i in inputName.indices) {
-                val listBytesSize = unpacking(inputName[i]).values.toList()
-                summarySize += listBytesSize[0]
-            }
-            listFileSizeByte.add(summarySize)
-            listFileName.add("Summary size")
-        }
-        for (i in listFileSizeByte.indices){
-            result.addAll(listOf("${listFileName[i]} - ${humanReadable(foundation, listFileSizeByte[i])}"))
+    if (totalSize) {
+        inputFile.forEach { getBytes(it).forEach { summarySize += it.value } }
+        if (humanReadable) result["Summary size"] = humanReadable(foundation, summarySize)
+        else {
+            val n = summarySize.toDouble() / baseFoundation.toDouble()
+            result["Summary size"] = "%.3f".format(n)
         }
     } else {
-
-        //здесь вывод в КВ
-        val baseFoundation = if (foundation) 1000 else 1024
-        for (i in inputName.indices) {
-            if (totalSize) {
-                summarySize += listFileSizeByte[i]
-            } else {
-                summarySize = listFileSizeByte[i]
+        for (i in inputFile.indices) {
+            val oneFileByteSize = getBytes(inputFile[i]).values.first()
+            if (humanReadable) result["${inputFile[i]}"] = humanReadable(foundation, oneFileByteSize)
+            else {
+                val n = oneFileByteSize.toDouble() / baseFoundation.toDouble()
+                result["${inputFile[i]}"] = "%.3f".format(n)
             }
-            val n = summarySize.toDouble() / baseFoundation.toDouble()
-            result.addAll(listOf("${listFileName[i]} - ${"%.3f".format(n)}"))
         }
-        if (totalSize) return listOf("Summary size - ${result[result.lastIndex].split(" ")[2]}")
     }
     return result
 }
@@ -53,7 +34,7 @@ fun humanReadable(foundation: Boolean, k: Long): String {
     var n = k.toDouble()
     val defaultFoundation = if (foundation) 1000.toDouble() else 1024.toDouble()
     if (n < defaultFoundation) return "$k B"
-    while (n.toLong() / defaultFoundation.toLong() > 0 && i < 4) {
+    while (n.toLong() / defaultFoundation.toLong() > 0 && i < type.size) {
         n /= defaultFoundation
         i += 1
     }
